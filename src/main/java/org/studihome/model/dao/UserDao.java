c3b050689e40bd38f14632d3d32f3185ff1d1b42
aschwanb <balz.aschwanden@students.unibe.ch>
2014-10-18 21:51:04 +0200

    Revert "Directory Structure. WIP"
    This reverts commit 262136944dce31af135b1cfc75d88abd6b9ec3aa.

--- src/main/java/org/studihome/model/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package org.studihome.model.dao;
-
-import org.studihome.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDao extends CrudRepository<User,Long> {
-}
