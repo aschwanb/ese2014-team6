49ca5e465348da82602d441663c56b7ab3ec4b1c
aschwanb <balz.aschwanden@students.unibe.ch>
2014-10-26 11:13:10 +0100

    Updat SRS with user descritption and use case diagram.

--- Skeleton/src/main/java/org/sample/controller/IndexController.java
+++ Skeleton/src/main/java/org/sample/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.sample.controller.pojos.SignupForm;
 import org.sample.controller.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,34 +22,22 @@ public class IndexController {
     SampleService sampleService;
 
 
-    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-    public ModelAndView index() {
-    	ModelAndView model = new ModelAndView("index");
-    	return model;
-    }
-    
-//    @RequestMapping(value = "/create", method = RequestMethod.POST)
-//    public ModelAndView create(@Valid SignupForm signupForm, BindingResult result, RedirectAttributes redirectAttributes) {
-//    	ModelAndView model;    	
-//    	if (!result.hasErrors()) {
-//            try {
-//            	sampleService.saveFrom(signupForm);
-//            	model = new ModelAndView("show");
-//            } catch (InvalidUserException e) {
-//            	model = new ModelAndView("index");
-//            	model.addObject("page_error", e.getMessage());
-//            }
-//        } else {
-//        	model = new ModelAndView("index");
-//        }   	
+//    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+//    public ModelAndView index() {
+//    	ModelAndView model = new ModelAndView("index");
 //    	return model;
 //    }
-//    
-//    @RequestMapping(value = "/security-error", method = RequestMethod.GET)
-//    public String securityError(RedirectAttributes redirectAttributes) {
-//        redirectAttributes.addFlashAttribute("page_error", "You do have have permission to do that!");
-//        return "redirect:/";
-//    }
+    
+	@RequestMapping(value={ "/", "/index"}, method=RequestMethod.GET)
+	public String loadIndex(ModelMap m) {
+		return "/index";
+	}
+
+    @RequestMapping(value = "/security-error", method = RequestMethod.GET)
+    public String securityError(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("page_error", "You do have have permission to do that!");
+        return "redirect:/";
+    }
 
 }
 
