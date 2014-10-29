6b5f415435432a3b8f5f350ed54cad9b86f980c0
aschwanb <balz.aschwanden@students.unibe.ch>
2014-10-29 16:00:49 +0100

    Skeleton for google map integration

---
+++ Skeleton/src/main/java/org/sample/controller/MapController.java
@@ -0,0 +1,30 @@
+package org.sample.controller;
+
+import javax.validation.Valid;
+
+import org.jboss.logging.Logger;
+import org.sample.controller.exceptions.InvalidUserException;
+import org.sample.controller.pojos.SignupForm;
+import org.sample.controller.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class MapController {
+
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+    public ModelAndView index() {
+    	ModelAndView model = new ModelAndView("map");
+    	return model;
+    }
+
+}
+
+
