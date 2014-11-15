b6854d68137ca20d6d17c84e4fdebc43810d4862
aschwanb <balz.aschwanden@students.unibe.ch>
2014-11-15 23:25:54 +0100

    Revert "GrantedAuthority is working now. Todo: Cleanup" Reason: My implementation was actually way to complicated.
    This reverts commit f0f3ed4d0adebb88b72035332816c46a8cd72aee.

--- Skeleton/src/main/java/ch/studihome/jspserver/model/User.java
+++ Skeleton/src/main/java/ch/studihome/jspserver/model/User.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
@@ -224,11 +223,7 @@ public class User implements UserDetails {
 	public boolean isEnabled() {return true;}
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 //		return (Collection<? extends GrantedAuthority>) userRoles;
-//		return null;
-		GrantedAuthority auth = new SimpleGrantedAuthority("ROLE_USER");
-		List<GrantedAuthority> colAuth = new ArrayList<GrantedAuthority>();
-		colAuth.add(auth);
-		return colAuth;
+		return null;
 	}
 
 	
