ae17afa3c7e2e8ba2a8e4ecd9a412682fc04fb87
Raoul Grossenbacher <ralle23091993@gmail.com>
2014-12-02 08:49:18 +0100

    AlertService started

--- Skeleton/src/main/java/ch/studihome/jspserver/controller/AlertController.java
+++ Skeleton/src/main/java/ch/studihome/jspserver/controller/AlertController.java
@@ -4,14 +4,17 @@ import java.util.HashSet;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.studihome.jspserver.controller.service.AlertService;
 import ch.studihome.jspserver.model.Alert;
 import ch.studihome.jspserver.model.User;
+import ch.studihome.jspserver.model.dao.UserDao;
 import ch.studihome.jspserver.model.pojos.SearchForm;
 
 /**
@@ -21,7 +24,10 @@ import ch.studihome.jspserver.model.pojos.SearchForm;
  */
 @Controller
 public class AlertController {
-
+	
+	@Autowired
+	AlertService alertService;
+	
 	/**
 	 * 
 	 * @return alerts view 
@@ -48,29 +54,10 @@ public class AlertController {
     	ModelAndView model = new ModelAndView("ajaxAnswer");
     	
         User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    	model.addObject("user", user);
     	
-    	//save alert to database
-    	HashSet<Alert> alerts = (HashSet<Alert>) user.getAlerts();
-    	Alert alert = new Alert();
-    	alert.setUser(user);
-    	alert.setMin_price(searchForm.getMin_price());
-    	alert.setMax_price(searchForm.getMax_price());
-    	alert.setMin_room_size(searchForm.getMin_room_size());
-    	alert.setMax_room_size(searchForm.getMax_room_size());
-    	alert.setMin_appartment_size(searchForm.getMin_appartment_size());
-    	alert.setMax_appartment_size(searchForm.getMax_appartment_size());
-    	alert.setMin_number_of_inhabitants(searchForm.getMin_number_of_inhabitants());
-    	alert.setMax_number_of_inhabitants(searchForm.getMax_number_of_inhabitants());
-    	alert.setArea(searchForm.getArea());
-    	alert.setType_of_apartment(searchForm.getType_of_apartment());
-    	
-    	alerts.add(alert);
-    	user.setAlerts(alerts);
-    	//TODO Save User
-    	
-    	
-    	model.addObject("alerts", "success");
+        alertService.addAlert(searchForm, user);
+        
+    	model.addObject("content", "success");
 
     	return model;
     }
---
+++ Skeleton/src/main/java/ch/studihome/jspserver/controller/service/AlertServiceImpl.java
@@ -0,0 +1,43 @@
+package ch.studihome.jspserver.controller.service;
+
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ch.studihome.jspserver.model.Alert;
+import ch.studihome.jspserver.model.User;
+import ch.studihome.jspserver.model.dao.UserDao;
+import ch.studihome.jspserver.model.pojos.SearchForm;
+
+public class AlertServiceImpl implements AlertService
+{
+	@Autowired
+	UserDao userDao;
+
+	public void addAlert(SearchForm searchForm, User user)
+	{
+		HashSet<Alert> alerts = (HashSet<Alert>) user.getAlerts();
+    	Alert alert = new Alert();
+    	alert.setUser(user);
+    	alert.setMin_price(searchForm.getMin_price());
+    	alert.setMax_price(searchForm.getMax_price());
+    	alert.setMin_room_size(searchForm.getMin_room_size());
+    	alert.setMax_room_size(searchForm.getMax_room_size());
+    	alert.setMin_appartment_size(searchForm.getMin_appartment_size());
+    	alert.setMax_appartment_size(searchForm.getMax_appartment_size());
+    	alert.setMin_number_of_inhabitants(searchForm.getMin_number_of_inhabitants());
+    	alert.setMax_number_of_inhabitants(searchForm.getMax_number_of_inhabitants());
+    	alert.setArea(searchForm.getArea());
+    	alert.setType_of_apartment(searchForm.getType_of_apartment());
+    	
+    	alerts.add(alert);
+    	userDao.save(user);
+	}
+
+	public void updateAlert(Alert alert)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
