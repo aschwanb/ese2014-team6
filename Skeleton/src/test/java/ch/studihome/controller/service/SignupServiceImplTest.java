6dae809e93aba93224b162657d37844c165b2d8b
Raoul Grossenbacher <ralle23091993@gmail.com>
2014-12-09 15:16:20 +0100

    SignupServiceImplTest finished Model + Service Tests finished

--- Skeleton/src/test/java/ch/studihome/controller/service/SignupServiceImplTest.java
+++ Skeleton/src/test/java/ch/studihome/controller/service/SignupServiceImplTest.java
@@ -3,11 +3,11 @@ package ch.studihome.controller.service;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 import ch.studihome.jspserver.controller.service.SignupServiceImpl;
 import ch.studihome.jspserver.model.User;
 import ch.studihome.jspserver.model.dao.AddressDao;
 import ch.studihome.jspserver.model.dao.UserDao;
+import ch.studihome.jspserver.model.pojos.ProfileForm;
 import ch.studihome.jspserver.model.pojos.SignupForm;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -49,6 +49,54 @@ public class SignupServiceImplTest
 		signupForm = signupServiceImpl.saveFrom(signupForm);
 		assertNotNull(signupForm.getId());
 		assertTrue(signupForm.getId() > 0);
+	}
+	
+	@Test
+	public void testUpdateFrom()
+	{
+		ProfileForm profileForm = new ProfileForm();
+		profileForm.setEmail("email");
+		profileForm.setFirstName("firstName");
+		profileForm.setLastName("lastName");
+		
+		User user = new User();
+		
+		signupServiceImpl.updateFrom(profileForm, user);
+		
+		when(userDao.save(any(User.class))).thenAnswer(new Answer<User> () {
+
+			public User answer(InvocationOnMock invocation) throws Throwable
+			{
+				User user = (User) invocation.getArguments()[0];
+				assertEquals("email", user.getEmail());
+				assertEquals("firstName", user.getFirstName());
+				assertEquals("lastName", user.getLastName());
+				return user;
+			}
+			
+		});
+	}
+	
+	@Test
+	public void testChangePasswordFrom()
+	{
+		String password = "password1";
+		User user = new User();
+		user.setPassword(password);
+		assertEquals(password, user.getPassword());
+		password = "password2";
+		signupServiceImpl.changePasswordFrom(password, user);
+		
+		when(userDao.save(any(User.class))).thenAnswer(new Answer<User> () {
+
+			public User answer(InvocationOnMock invocation) throws Throwable
+			{
+				User user = (User) invocation.getArguments()[0];
+				assertEquals("password2", user.getPassword());
+				return user;
+			}
+			
+		});
 	}
 
 }
